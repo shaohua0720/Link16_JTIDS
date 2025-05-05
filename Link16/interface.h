aa366b92b6a49b7b7eebbaf964d9073cdae0875e
User <user@example.com>
2025-05-05 10:48:00 +0800

    删除原始Link16文件夹，完成项目重构

--- Link16/interface.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-#include "tools.h"
-#include "global.h"
-
-#include "decodeTools.h"
-#include "STDPMsg.h"
-
-int32_t encoder_Link16(int32_t n, int32_t m, string& inputs);
-int32_t decoder_Link16(string& raw_data, int32_t& n, int32_t& m);
